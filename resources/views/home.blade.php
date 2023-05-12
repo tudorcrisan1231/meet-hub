399c15d77a95b8af2ba601f8eeff13a31bd9b914
tudorcrisan1231 <crisantudor79@yahoo.com>
2023-05-12 23:30:37 +0300

    map

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,52 @@
 
 @section('content')
     <div>{{auth()->user()->name}}</div>
-    <a class="nav-link" href="{{ route('signout') }}">Logout</a
+    <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+    <div id="map" style="height: 380px"></div>
+
+    <script>
+        function getCurrentCoordinates() {
+            return new Promise((resolve, reject) => {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        (position) => {
+                        const latitude = position.coords.latitude;
+                        const longitude = position.coords.longitude;
+                        resolve({ latitude, longitude });
+                        },
+                        (error) => {
+                        reject(error);
+                        }
+                    );
+                } else {
+                    reject(new Error('Geolocation is not supported by this browser.'));
+                }
+            });
+        }
+
+        getCurrentCoordinates()
+        .then((coordinates) => {
+            const latitude = coordinates.latitude;
+            const longitude = coordinates.longitude;
+
+            var map = L.map('map').setView([latitude, longitude], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker([latitude, longitude]).addTo(map)
+                .bindPopup('Locatia ta')
+                .openPopup();
+        }) //in caz ca nu merge locatia o pun eu hardcodata
+        .catch((error) => {
+            var map = L.map('map').setView([45.7458483, 21.2403663], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker([45.7458483, 21.2403663]).addTo(map)
+                .bindPopup('Complexul studentilor')
+                .openPopup();
+        });
+    </script>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,10 +12,19 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+        crossorigin=""/>
+        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+        integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+        crossorigin=""></script>
+        
+        @livewireStyles
     </head>
     <body>
         @yield('content')
 
+        @livewireScripts
         <script src="js/app.js"></script>
     </body>
 </html>
