098cca194eb492b34595c213dc349bb566864251
Razvan Pasaran <razvanandreipasaran@gmail.com>
2023-05-13 01:34:46 +0300

    Login - Register Design
    sdklfsd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,44 +5,48 @@
     <div class="cotainer-login">
         <div class="row justify-content-center">
             <div class="col-md-4">
-                <div class="card">
-                    <h3 class="title-login">Login</h3>
+                <div class="card">!
+                    <h3 class="register-login-title">Welcome 😄</h3>
                     <div class="form-login">
                         <form method="POST" action="{{ route('login.custom') }}">
                             @csrf
                             <div class="form-group mb-3">
-                                <input  type="text" placeholder="Email" id="email" class="input-login" name="email" required
+                                <input  type="text" placeholder="Email" id="email" class="input" name="email" required
                                     autofocus>
                                 @if ($errors->has('email'))
                                 <span class="text-danger">{{ $errors->first('email') }}</span>
                                 @endif
                             </div>
                             <div class="form-group mb-3">
-                                <input type="password" placeholder="Password" id="password" class="input-login" name="password" required>
+                                <input type="password" placeholder="Password" id="password" class="input" name="password" required>
                                 @if ($errors->has('password'))
                                 <span class="text-danger">{{ $errors->first('password') }}</span>
                                 @endif
                             </div>
                             <div class="form-group mb-3">
                                 <div class="checkbox">
+                                
                                     <label>
-                                        <input type="checkbox" name="remember"> Remember Me
+                                        <input type="checkbox" class="check" name="remember"> Remember Me
                                     </label>
+                                    <div class="d-grid mx-auto">
+                                
+                            </div>
                                 </div>
+                                <button type="submit" class="button_login_register">Login</button>
                             </div>
-                            <div class="d-grid mx-auto">
-                                <button type="submit" class="button-login">Signin</button>
-                            </div>
+                            
                         </form>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="already-an-account">
-            Already an account?
+                </div>
+    <div class="already-an-account">
+            Don't have an account?
         <a class="register-login" href="{{route('register-user')}}">REGISTER</a>
         </div>
-        
+        <div>
+            <p class="Terms">Read our terms and conditions <span>Click</span></p>
+        </div>
     </div>
 </main>
 @endsection
--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.main')
 @section('content')
 <main class="main-container ">
-    <div class="cotainer">
+    <h1 class="logo">MeetHub</h1>
+    <div class="cotainer-login">
         <div class="row justify-content-center">
             <div class="col-md-4">
                 <div class="card">
-                    <h3 class="card-header text-center">Register User</h3>
-                    <div class="card-body">
+                    <h3 class="register-login-title">Register User</h3>
+                    <div class="form-login">
                         <form action="{{ route('register.custom') }}" method="POST">
                             @csrf
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Name" id="name" class="form-control" name="name"
+                                <input type="text" placeholder="Name" id="name" class="input" name="name"
                                     required autofocus>
                                 @if ($errors->has('name'))
                                 <span class="text-danger">{{ $errors->first('name') }}</span>
                                 @endif
                             </div>
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Email" id="email_address" class="form-control"
+                                <input type="text" placeholder="Email" id="email_address" class="input"
                                     name="email" required autofocus>
                                 @if ($errors->has('email'))
                                 <span class="text-danger">{{ $errors->first('email') }}</span>
                                 @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="password" placeholder="Password" id="password" class="form-control"
-                                    name="password" required>
-                                @if ($errors->has('password'))
-                                <span class="text-danger">{{ $errors->first('password') }}</span>
-                                @endif
-                            </div>
+                                </div>
                             <div class="form-group mb-3">
                                 <div class="checkbox">
-                                    <label><input type="checkbox" name="remember"> Remember Me</label>
-                                </div>
+                                
+                                    <label>
+                                        <input type="checkbox" class="check" name="remember"> Remember Me
+                                    </label>
+                                    <div class="d-grid mx-auto">
+                                
                             </div>
-                            <div class="d-grid mx-auto">
-                                <button type="submit" class="btn btn-dark btn-block">Sign up</button>
+                                </div>
+                                <button type="submit" class="button_login_register">SignUp</button>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="already-an-account">
+            Already an account?
+        <a class="register-login" href="{{route('login')}}">LOGIN</a>
+        </div>
+        <div>
+            <p class="Terms">Read our terms and conditions <span>Click</span></p>
+        </div>
     </div>
-    <a href="{{route('login')}}">login</a>
+    </div>
+    
 </main>
 @endsection
