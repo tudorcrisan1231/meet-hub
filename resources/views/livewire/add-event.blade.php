8152a7f662be03600536898019a747c0d43f845c
Razvan Pasaran <razvanandreipasaran@gmail.com>
2023-05-13 17:23:23 +0300

    Profile - design - final
    Pr

--- resources/views/livewire/add-event.blade.php
+++ resources/views/livewire/add-event.blade.php
@@ -1,41 +1,44 @@
-<div class="" style="padding-bottom: 5rem;">
-    <div class="register-login-title">Creaza un eveniment</div>
+<div class="main_container_fluid" style="padding-bottom: 5rem;">
+<div class="profile_btn-nav">
+<a href="{{route('home')}}" class="profile_btn-nav_link" id="logout_profile" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10.875 19.3l-6.6-6.6q-.15-.15-.213-.325T4 12q0-.2.063-.375t.212-.325l6.6-6.6q.275-.275.688-.287t.712.287q.3.275.313.688T12.3 6.1L7.4 11h11.175q.425 0 .713.288t.287.712q0 .425-.287.713t-.713.287H7.4l4.9 4.9q.275.275.288.7t-.288.7q-.275.3-.7.3t-.725-.3Z"/></svg>
+</a>
+</div>
+    <div class="create_title">Create event</div>
 
 
 
     <div class="profile_group_label">
-        <label class="profile_text" for="event_name">Numele eventului</label>
+        <label class="profile_text" for="event_name">Event name</label>
         <input type="text" id="event_name" wire:model="event_name" class="input">
     </div>
 
-    <div>
-        <label for="event_description">descrierea ta</label>
-        <textarea name="" id="event_description" cols="30" rows="10" wire:model="event_description" class="input-login"></textarea>
-    </div>
-
     <div class="profile_group_label">
-        <label class="profile_text" for="event_theme">Tematica</label>
+        <label class="profile_text" for="profile_about">Event Description</label>
+        <textarea name="" id="profile_about" cols="30" rows="10" wire:model="event_description" class="input select"></textarea>
+    </div>
+    <div class="profile_group_label">
+        <label class="profile_text" for="event_theme">Theme</label>
         <input type="text" id="event_theme" wire:model="event_theme" class="input">
     </div>
 
     <div class="profile_group_label">
-        <label class="profile_text" for="event_date">Data cand are loc</label>
+        <label class="profile_text" for="event_date">Date</label>
         <input type="date" id="event_date" wire:model="event_date" class="input">
     </div>
 
     <div class="profile_group_label">
-        <label class="profile_text" for="event_time">Durata evenutului (in ore)</label>
+        <label class="profile_text" for="event_time">Duration of the event (hours)</label>
         <input type="text" id="event_time" wire:model="event_time" class="input">
     </div>
 
 
     <div class="profile_group_label">
-        <label class="profile_text" for="event_limit">Limita de persoane</label>
+        <label class="profile_text" for="event_limit">The number of people</label>
         <input type="number" id="event_limit" wire:model="event_limit" class="input">
     </div>
 
     <div class="profile_group_label">
-        <label class="profile_text" for="event_img">Imaginea principala</label>
+        <label class="profile_text" for="event_img">Main image</label>
         <input type="file" id="event_img" wire:model="event_img" class="input">
     </div>
 
