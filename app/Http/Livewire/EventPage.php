5d3c51ca7eac48e495ad17fa948a311d76dccd48
tudorcrisan1231 <crisantudor79@yahoo.com>
2023-05-13 19:25:10 +0300

    event page not done

--- app/Http/Livewire/EventPage.php
+++ app/Http/Livewire/EventPage.php
@@ -2,13 +2,47 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Event;
+use App\Models\User;
 use Livewire\Component;
 
 class EventPage extends Component
 {
-    public $event_id;
+    public $event_id, $event, $geo_location, $organizator, $participants;
+
     public function render()
     {
+        $this->event = Event::find($this->event_id);
+        $this->organizator = User::find($this->event->organizer);
+        $this->participants = User::whereIn('id', json_decode($this->event->users))->get();
+
+        $coords = json_decode($this->event->location);
+        $apiKey = 'f303ee4df8a344528a177207988f6d33';
+
+        $url = "https://api.opencagedata.com/geocode/v1/json?q={$coords[0]}+{$coords[1]}&key={$apiKey}";
+        
+        $response = file_get_contents($url);
+        $result = json_decode($response, true);
+        $this->geo_location = $result['results'][0]['formatted'];
+
         return view('livewire.event-page');
+    }
+
+    public function participate(){
+        $event = Event::find($this->event_id);
+        $users = json_decode($event->users);
+        $users[] = auth()->user()->id;
+        $event->users = json_encode($users);
+        $event->save();
+        return redirect()->to('/event/'.$this->event_id);
+    }
+
+    public function unparticipate(){
+        $event = Event::find($this->event_id);
+        $users = json_decode($event->users);
+        $users = array_diff($users, [auth()->user()->id]);
+        $event->users = json_encode($users);
+        $event->save();
+        return redirect()->to('/event/'.$this->event_id);
     }
 }
--- resources/views/livewire/event-page.blade.php
+++ resources/views/livewire/event-page.blade.php
@@ -1,3 +1,67 @@
-<div>
-    {{$event_id}}
+<div style="font-size: 1.6rem; color:white;">
+    @if($event->image)
+        <img src="{{asset('storage/'.$event->image)}}" alt="" style="width: 100%;">
+    @else
+        <img src="https://theperfectroundgolf.com/wp-content/uploads/2022/04/placeholder.png" alt="" style="width: 100%;">
+    @endif
+   
+    <div>Nume: {{$event->name}}</div>
+    <div>Descriere: {{$event->description}}</div>
+    <div>Locatie: {{$geo_location}}</div>
+
+    @php
+        //calculate the diffrence in days and hours between the current date and the event date
+        $date1 = strtotime($event->date);
+        $date2 = strtotime(date("Y-m-d"));
+        $diff = abs($date1 - $date2);
+        $hours = floor($diff / (60*60));
+        $diff = floor($diff / (60*60*24));
+    @endphp
+    <div>Incepe in: {{$event->date}} la {{$event->hour}} (in {{$diff}} zile SAU {{$hours}} ore)</div>
+
+    <div>Durata: {{$event->time}} ore</div>
+
+    <div>Limita: {{$event->limit}} persoane</div>
+
+    <div>Tema: {{$event->type}}</div>
+
+    <div>Organizator: 
+        <div><img src="{{asset('storage/'.$organizator->photo)}}" alt=""></div>
+        <div>{{$organizator->name}}</div>
+    </div>
+
+    <div>Participanti: 
+        <div>
+            @foreach ($participants as $participant)
+                @if($participant->photo)
+                    <div><img width="200" src="{{asset('storage/'.$participant->photo)}}" alt=""></div>
+                @else
+                    <div><img width="200" src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" alt=""></div>
+                @endif
+                
+                <div>nume: {{$participant->name}}</div>
+            @endforeach
+        </div>
+    </div>
+
+    @php
+        $participate = false;
+        foreach ($participants as $participant) {
+            if($participant->id == auth()->user()->id){
+                $participate = true;
+            }
+        }
+    @endphp
+    @if(auth()->user()->id != $organizator->id && !$participate)
+        <div class="button_login_register" wire:click="participate">
+            Participa
+        </div>
+    @else
+        <div class="button_login_register" wire:click="unparticipate">
+            Nu mai participa
+        </div>
+        <div class="button_login_register" wire:click="openChat">
+            Open chat
+        </div>
+    @endif
 </div>
