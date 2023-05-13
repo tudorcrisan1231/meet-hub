dcd05e9304c340c27158bb9c9cbc2538aa6dc863
tudorcrisan1231 <crisantudor79@yahoo.com>
2023-05-13 17:23:27 +0300

    render events on map + sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,13 +19,71 @@
             @endforeach
         </div>
         <div class="card_btns">
-            <div class="card_btns_btn button_login_register"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M4 18q-.425 0-.713-.288T3 17q0-.425.288-.713T4 16h16q.425 0 .713.288T21 17q0 .425-.288.713T20 18H4Zm0-5q-.425 0-.713-.288T3 12q0-.425.288-.713T4 11h16q.425 0 .713.288T21 12q0 .425-.288.713T20 13H4Zm0-5q-.425 0-.713-.288T3 7q0-.425.288-.713T4 6h16q.425 0 .713.288T21 7q0 .425-.288.713T20 8H4Z"/></svg></div>
+            <div class="card_btns_btn button_login_register card_btns_open"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M4 18q-.425 0-.713-.288T3 17q0-.425.288-.713T4 16h16q.425 0 .713.288T21 17q0 .425-.288.713T20 18H4Zm0-5q-.425 0-.713-.288T3 12q0-.425.288-.713T4 11h16q.425 0 .713.288T21 12q0 .425-.288.713T20 13H4Zm0-5q-.425 0-.713-.288T3 7q0-.425.288-.713T4 6h16q.425 0 .713.288T21 7q0 .425-.288.713T20 8H4Z"/></svg></div>
             <div class="card_btns_btn button_login_register">Events</div>
             <a href="{{route('add_event')}}" class="card_btns_btn button_login_register">Create</a>
         </div>
     </div>
+
+    <div class="sidebar">
+        <div>salut</div>
+        <div>salut</div>
+        <div>salut</div>
+
+        <div class="sidebar_close">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275L12 13.4Z"/></svg>
+        </div>
+    </div>
 </div>
 <style>
+    .sidebar{
+        position: fixed;
+        background-color: white;
+        top: 0;
+        left: -100%;
+        width: 40%;
+        height: 100%;
+        z-index: 999;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border-right: 1px solid #ccc;
+        transition: all .2s;
+    }
+    .sidebar_active{
+        left: 0;
+    }
+    .sidebar_close{
+        position: absolute;
+        right: 1rem;
+        top: 1rem;
+    }
+    .sidebar_close svg{
+        width: 30px;
+        height: 30px;
+        cursor: pointer;
+    }
+    .cur-popup{
+        background-color: #222327;
+        border-radius: 5px;
+        color: white;
+    }
+    .leaflet-popup-tip{
+        background-color: #222327;
+        border: none;
+       
+    }
+    .leaflet-popup-content-wrapper{
+        background-color: transparent;
+        border: none;
+        border-radius: 0;
+    }
+    .leaflet-popup-content{
+        color: white;
+        font-size: 1.4rem;
+    }
     .card{
         position: absolute;
         bottom: 0;
@@ -66,6 +124,16 @@
 </style>
 
 <script>
+    const card_btns_open = document.querySelector('.card_btns_open');
+    const sidebar = document.querySelector('.sidebar');
+    const sidebar_close = document.querySelector('.sidebar_close');
+    card_btns_open.addEventListener('click', () => {
+        sidebar.classList.add('sidebar_active');
+    });
+    sidebar_close.addEventListener('click', () => {
+        sidebar.classList.remove('sidebar_active');
+    });
+
     function getCurrentCoordinates() {
         return new Promise((resolve, reject) => {
             if (navigator.geolocation) {
@@ -90,16 +158,29 @@
         const latitude = coordinates.latitude;
         const longitude = coordinates.longitude;
 
-        var map = L.map('map').setView([latitude, longitude], 13);
+        var map = L.map('map').setView([latitude, longitude], 14);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([latitude, longitude]).addTo(map)
-            .bindPopup('Locatia ta')
+        
+       //render events on map
+        @foreach ($events as $event)
+            lat = {{json_decode($event->location)[0]}};
+            lng = {{json_decode($event->location)[1]}};
+            L.marker([lat, lng]).addTo(map)
+            .bindPopup(L.popup({
+                autoClose:false,
+                closeOnClick: false,
+                className: 'cur-popup',
+            })
+            )
+            .setPopupContent('<div class="cur-popup"><h3>{{$event->name}}</h3></div>')
             .openPopup();
 
-
+            map.setView([lat,lng]);
+        @endforeach
+      
     }) //in caz ca nu merge locatia o pun eu hardcodata
     .catch((error) => {
         var map = L.map('map').setView([45.7458483, 21.2403663], 13);
